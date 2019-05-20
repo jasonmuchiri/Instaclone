ea50a5b1afbc6488f01e3d9b4ed70eb29de02053
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-20 12:06:48 +0300

    Define placeholders,variables and bits of logic that regulate our templates

--- instaclone/views.py
+++ instaclone/views.py
@@ -3,3 +3,5 @@ from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
--- instagram/urls.py
+++ instagram/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^instaclone/',include('instaclone.urls')),
+    url(r'',include('instaclone.urls')),
 ]
