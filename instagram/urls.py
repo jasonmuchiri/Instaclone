1c045f4b4e5c6f010b42f1652226c7a93549f7f3
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-21 18:02:50 +0300

    Update the registration templates and the base and home templates and also the project's urls.py file

--- instagram/urls.py
+++ instagram/urls.py
@@ -20,5 +20,5 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('instaclone.urls')),
-    
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
